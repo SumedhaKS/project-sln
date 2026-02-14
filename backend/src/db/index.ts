40def7a753ca96af8e6bea4f6755f0facb258b94
SumedhaKS <kssumedha24@gmail.com>
2026-02-14 17:15:18 +0530

    init

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,7 +1,12 @@
-import { PrismaClient } from "@prisma/client/extension"
+import "dotenv/config";
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from './generated/prisma/client'
 
-const prismaClientSingleton = ()=>{
-    return new PrismaClient();
+const prismaClientSingleton = () => {
+    const connectionString = `${process.env.DATABASE_URL}`
+    const adapter = new PrismaPg({ connectionString })
+    const prisma = new PrismaClient({ adapter })
+    return prisma
 }
 
 type prismaClientSingleton = ReturnType<typeof prismaClientSingleton>
