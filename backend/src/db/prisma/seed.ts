76fc32180fd6f8951e0d169cb6473dda426ed88d
SumedhaKS <kssumedha24@gmail.com>
2026-02-14 17:15:45 +0530

    seed

--- backend/src/db/prisma/seed.ts
+++ backend/src/db/prisma/seed.ts
@@ -1,1 +1,28 @@
 // to seed data
+
+
+import bcrypt from "bcrypt";
+import prisma from "..";
+
+async function main() {
+    try {
+        const admin = await prisma.user.upsert({
+            where: {username: "admin"},
+            update: {},
+            create: {
+                username: "admin",
+                password: await bcrypt.hash('secretadmin', 10)
+            },
+        })
+
+        console.log(`Admin seeded successfully: ${admin.username}`);
+    }
+    catch (err) {
+        console.error(`Failed to Seed.\nError: ${err}`);
+    }
+    finally {
+        await prisma.$disconnect();
+    }
+}
+
+main()
